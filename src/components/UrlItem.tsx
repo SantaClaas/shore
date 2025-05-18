6fbe150d50ac51e6ee5d314670a503f695349562
Claas <claas@claas.dev>
2025-05-18 20:29:35 +0200

    Prefer pattern

--- src/components/UrlItem.tsx
+++ src/components/UrlItem.tsx
@@ -1,19 +1,18 @@
-import { createResource } from "solid-js";
+import { createEffect, createResource } from "solid-js";
 import getDocument from "../getDocument";
 
 export default function UrlItem({ url }: { url: URL }) {
   const [document] = createResource(url, getDocument);
 
-  const title = () => document()?.title ?? url.hostname;
-  // classList={{
-  //   "relative  after:absolute after:inset-0 after:bg-[url(/public/diagonal-stripes.svg)] after:animate-stripes *:opacity-70 after:starting:opacity-0 after:transition-all after:transition-discrete after:duration-1000":
-  //     document.loading,
-  // }}
+  const title = () => document()?.title || url.hostname;
+
   return (
     <a
       href={url.href}
+      target="_blank"
+      rel="noreferrer"
       data-is-loading={document.loading || undefined}
-      class="group relative flex overflow-clip flex-col items-center justify-around p-4 size-28 border-4 rounded-3xl focus-visible:border-orange-500 focus-visible:text-orange-500 focus:outline-none text-center align-middle content-center data-is-loading:border-stone-400 "
+      class="group relative flex overflow-clip flex-col items-center justify-around p-4 size-28 border-4 rounded-3xl focus-visible:border-orange-500 focus-visible:text-orange-500 focus:outline-none text-center align-middle content-center data-is-loading:border-stone-900 outline-transparent hover:outline-stone-900 -outline-offset-4 hover:-outline-offset-6 outline-4 outline-solid transition-[outline]"
     >
       {/* <img
                   src={`https://icons.duckduckgo.com/ip3/${destination.url.hostname}.ico`}
@@ -27,9 +26,10 @@ export default function UrlItem({ url }: { url: URL }) {
       />
       <span>{title()}</span>
 
+      {/* Pattern based on tailwindcss.com */}
       <div
         data-name="stripes-animation"
-        class="hidden group-data-is-loading:block absolute inset-0 bg-[url(/public/diagonal-stripes.svg)] animate-stripes opacity-0 group-data-is-loading:opacity-100 text-stone-300 starting:opacity-0 transition-[display_opacity] duration-1000 transition-discrete"
+        class="hidden group-data-is-loading:block absolute inset-0 bg-[image:repeating-linear-gradient(45deg,_var(--pattern-fg)_0,_var(--pattern-fg)_4px,_transparent_0,_transparent_50%)] bg-[size:40px_40px] bg-fixed [--pattern-fg:var(--color-stone-900)] animate-stripes opacity-0 group-data-is-loading:opacity-100 text-stone-300 starting:opacity-0 transition-[display_opacity] duration-1000 transition-discrete"
       />
     </a>
   );
